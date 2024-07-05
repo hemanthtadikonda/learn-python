b74dbbdd44c085a31f05a0d40890e89d674cb92d
hemanth <hemanthtadikonda29@gmail.com>
2024-07-05 11:29:27 +0530

    list & tuples in python

--- learn-python/10.list.py
+++ learn-python/10.list.py
@@ -33,3 +33,63 @@ for row in matrix:
     for item in row:
         print(item)
 
+
+### List Methods
+numbers = [ 2,3,4,9,7,5,10,8 ]
+
+numbers.append(6)
+print(numbers)
+
+numbers.insert(2,15) # deals with index.
+print(numbers)
+
+numbers.remove(5)
+print(numbers)
+
+numbers.pop()  # remove last number in list
+print(numbers)
+
+numbers.clear() # remove content in list
+print(numbers)
+
+####
+numbers = [ 2,3,4,9,7,5,10,8,5 ]
+
+print(numbers.index(4))
+# print(numbers.index(11)) value error exit with code 1
+
+print(11 in numbers)  # provide boolean output
+print(10 in numbers)
+
+
+print(numbers.count(5)) # count num of characters in that list
+
+numbers.sort()
+print(numbers)
+
+numbers.reverse()
+print(numbers)
+
+numbers2 = numbers.copy()
+numbers.append(11)
+print(numbers)
+print(numbers2)
+
+#### To remove duplicates in list
+
+numbers = [ 2,3,4,2,9,7,5,10,8,5 ]
+
+uniques = []
+
+for num in numbers:
+    if num not in uniques:
+        uniques.append(num)
+print(uniques)
+
+
+
+
+
+
+
+
---
+++ learn-python/tuples.py
@@ -0,0 +1,11 @@
+numbers = ( 1,2,3,4)
+
+# numbers.append(5)  *** We can't modify tuples those are immutable ***
+# numbers[0] = 10
+# print(numbers)
+
+# unpacking  { this method can use both list & tuples
+coordinates =( 1,2,3)
+x,y,z = coordinates
+
+print(x,z,y)
