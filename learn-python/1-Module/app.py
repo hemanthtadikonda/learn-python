3d5827e852fff9473f73380d94fe8356306bc904
hemanth <hemanthtadikonda29@gmail.com>
2024-07-07 16:20:36 +0530

    Modules in python

---
+++ learn-python/1-Module/app.py
@@ -0,0 +1,16 @@
+import converter
+print(converter.lbs_to_kgs(70))
+
+
+from converter import kgs_to_lbs
+print(kgs_to_lbs(72))
+
+
+from utils import find_max
+
+numbers = [ 1,3,5,7,8,9]
+print(find_max(numbers))
+
+import utils
+numbers = [ 1,3,5,7,8,9,11,12]
+print(find_max(numbers))
